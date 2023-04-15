0d1b91aa6c8c394454cd9f603e0f3e961d85dc24
Jacek Dajda <dajda@agh.edu.pl>
2023-04-15 03:38:29 +0200

    Prepared lab2 init

---
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -0,0 +1,74 @@
+package com.company.enroller.persistence;
+
+import java.util.Collection;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Component;
+
+import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
+
+@Component("meetingService")
+public class MeetingService {
+
+	Session session;
+
+	public MeetingService() {
+		session = DatabaseConnector.getInstance().getSession();
+	}
+
+	public Collection<Meeting> getAll() {
+		String hql = "FROM Meeting";
+		Query query = this.session.createQuery(hql);
+		return query.list();
+	}
+
+	public Meeting findById(long id) {
+		return this.session.get(Meeting.class, id);
+	}
+
+	public Collection<Meeting> findMeetings(String title, String description, Participant participant, String sortMode) {
+		String hql = "FROM Meeting as meeting WHERE title LIKE :title AND description LIKE :description ";
+		if (participant!=null) {
+			hql += " AND :participant in elements(participants)";
+		}
+		if (sortMode.equals("title")) {
+			hql += " ORDER BY title";
+		}
+		Query query = this.session.createQuery(hql);
+		query.setParameter("title", "%" + title + "%").setParameter("description", "%" + description + "%");
+		if (participant!=null) {
+			query.setParameter("participant", participant);
+		}
+		return query.list();
+	}
+
+	public void delete(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		this.session.delete(meeting);
+		transaction.commit();
+	}
+
+	public void add(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		this.session.save(meeting);
+		transaction.commit();
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		this.session.merge(meeting);
+		transaction.commit();
+	}
+
+	public boolean alreadyExist(Meeting meeting) {
+		String hql = "FROM Meeting WHERE title=:title AND date=:date";
+		Query query = this.session.createQuery(hql);
+		Collection resultList = query.setParameter("title", meeting.getTitle()).setParameter("date", meeting.getDate())
+				.list();
+		return query.list().size() != 0;
+	}
+
+}
